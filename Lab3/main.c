494dee2c5e778435056a5629b705c6ed6f0dfe3d
Saleem <msaleem@iastate.edu>
2026-02-16 22:27:02 -0600

    lab3 now points in right direction

--- Lab3/main.c
+++ Lab3/main.c
@@ -4,6 +4,9 @@
 #include "lcd.h"
 #include "stdio.h"
 #include "objects.h"
+#include "movement.h"
+#include "open_interface.h"
+#include <math.h>
 
 /*
 Author: Muhammad Raham Saleem. Reid Post
@@ -35,6 +38,9 @@ int main(void){
     lcd_init();
     cyBOT_init_Scan(0b0111);
     cyBot_uart_init();
+    oi_t *sensor_data = oi_alloc(); // do this only once at start of main()
+    oi_init(sensor_data); // do this only once at start of main()
+
 
 
 //variables
@@ -44,8 +50,8 @@ int main(void){
 
 
     // manual calibration of left/right values
-    right_calibration_value = 290500;
-    left_calibration_value = 1209250;
+    right_calibration_value = 316750;
+    left_calibration_value = 1298500;
 
     cyBOT_Scan_t scan;
 
@@ -54,6 +60,10 @@ while(1){
     char c = cyBot_getByte(); //blocks until key is pressed
     lcd_printf("Got a %c", c); //show recieved char on LCD
     cyBot_sendString("Got a key \n\r"); //sends message back to putty
+    if(c == ' '){
+        break; //
+    }
+
     if(c=='m'){
         //if m is pressed then scan is performed
         //scans the arm in 2 degree increments and writes and prints data
@@ -69,8 +79,15 @@ while(1){
 
             cyBot_sendString("\n\r=== Starting Scan ===\n\r");
 
-        for(i=60;i<120;i++){
+            int imax = 182;
 
+            cyBOT_Scan(0, &scan);
+//            prevDist = currDist;
+
+            prevDist = scan.sound_dist;
+            currDist = scan.sound_dist;
+
+        for(i=1;i<imax;i += 1){//we already scanned 0
             cyBOT_Scan(i, &scan);
 
             prevDist=currDist;
@@ -88,7 +105,7 @@ while(1){
             cyBot_sendString("\n\r");
 
             // Detect START of object: distance suddenly drops
-               if(!inObject && (prevDist - currDist > 50)) {
+               if(!inObject && (prevDist - currDist > 15&& currDist < 100)) {  // Added currDist < 100 to ignore walls and noise
                    inObject = true;
                    obj[objCounter].startAngle = i;
                    objectDistSum = currDist;
@@ -99,18 +116,19 @@ while(1){
                    cyBot_sendString(" (distance drop detected)\n\r");
                }
                // Still in object: accumulate distance
-               else if(inObject && currDist < 250) {
+//               else if(inObject && fabs(currDist - prevDist) < 10) { //changed this to look at slope too
+               else if (inObject&& currDist < 200){
                    objectDistSum += currDist;
                    objectScanCount++;
                }
                // Detect END of object: distance suddenly increases
-               if(inObject && (prevDist - currDist < -30)) {
-                   obj[objCounter].endAngle = i - 2;
+               if(inObject && (currDist - prevDist > 15)) {
+                   obj[objCounter].endAngle = i - 1;
                    obj[objCounter].distance = objectDistSum / objectScanCount;
                    obj[objCounter].radialWidth = obj[objCounter].endAngle - obj[objCounter].startAngle;
                    obj[objCounter].middleAngle = (obj[objCounter].startAngle + obj[objCounter].endAngle) / 2;
 
-                   if(obj[objCounter].radialWidth > 3) {
+                   if(obj[objCounter].radialWidth > 5) {
                    cyBot_sendString("<<< OBJECT END at angle ");
                    cyBot_sendFloat(i);
                    cyBot_sendString(" (distance jump detected)\n\r");
@@ -140,54 +158,53 @@ while(1){
                } else {
                    cyBot_sendString("!!! Ignoring noise (width too small)\n\r");
                }
+
+
                    //reset
                    inObject = false;
                    objectDistSum = 0;
                    objectScanCount = 0;
            }
+               prevDist=currDist;
                lcd_printf("Angle: %d\nDist: %.2f", i, scan.sound_dist);
-        }//end of for loop
-               //
+            }//end of for loop
+               // handling obj that extends end of scan also avoid background
+        if(inObject && (objectDistSum / objectScanCount) < 100) {
+            obj[objCounter].endAngle = imax - 1;
+            obj[objCounter].distance = objectDistSum / objectScanCount;
+            obj[objCounter].radialWidth = obj[objCounter].endAngle - obj[objCounter].startAngle;
+            obj[objCounter].middleAngle = (obj[objCounter].startAngle + obj[objCounter].endAngle) / 2.0;
 
-//            //if(prevDist-currDist>30){
-//                obj[objCounter].startAngle=i;
-//
-////                lcd_printf("object detected");
-//                cyBot_sendString("\n object detected at ");
-//                cyBot_sendFloat(currDist);
-//                cyBot_sendString("\n\r");
-//                cyBot_sendString("Angle detected at \n\r");
-//                cyBot_sendFloat(i);
-////                char angle[] = {};
-////                angle = i;
-//
-//            //}
-//                if(prevDist-currDist<-30){
-//                    cyBot_sendString("object ended \n\r");
-//                    obj[objCounter].endAngle=i;
-//
-//
-//                    objCounter++;
-//                }
-//                if(objCounter>0){
-//                cyBot_sendString("Object ");
-//                cyBot_sendFloat(objCounter);
-//                cyBot_sendString("\nstartAngle:");
-//                cyBot_sendFloat(obj[objCounter-1].startAngle);
-//                cyBot_sendString("\n\r");
-//                cyBot_sendString("endAngle:");
-//                cyBot_sendFloat(obj[objCounter-1].endAngle);
-//                cyBot_sendString("\n\r");
-//                char scanDistance[32] = {};
-//                sprintf(scanDistance, "%f\n\r", scan.sound_dist);
-//                 cyBot_sendString(scanDistance);
-//                 //            char angle[] ={};
-////            sprintf(angle, "%i\t",i);
-////            cyBot_sendString(angle);
-////
-////            char scanDistance[] = {};
-////            sprintf(scanDistance, "%f\n\r", scan.sound_dist);
-////                    cyBot_sendString(scanDistance);
+            if(obj[objCounter].radialWidth > 5) {
+                cyBot_sendString("<<< OBJECT END at scan limit\n\r");
+
+                cyBot_sendString("\n--- Object #");
+                cyBot_sendFloat(objCounter + 1);
+                cyBot_sendString(" Summary ---\n\r");
+                cyBot_sendString("  Start Angle: ");
+                cyBot_sendFloat(obj[objCounter].startAngle);
+                cyBot_sendString("\n\r  End Angle: ");
+                cyBot_sendFloat(obj[objCounter].endAngle);
+                cyBot_sendString("\n\r  Middle Angle: ");
+                cyBot_sendFloat(obj[objCounter].middleAngle);
+                cyBot_sendString("\n\r  Distance: ");
+                cyBot_sendFloat(obj[objCounter].distance);
+                cyBot_sendString(" cm\n\r");
+                cyBot_sendString("  Radial Width: ");
+                cyBot_sendFloat(obj[objCounter].radialWidth);
+                cyBot_sendString(" degrees\n\r\n\r");
+
+                objCounter++;
+            }
+        }
+
+        // Now find smallest object after scan is complete
+        cyBot_sendString("\n\r=== SCAN COMPLETE ===\n\r");
+
+        // debug output
+        cyBot_sendString("Total objects detected: ");
+        cyBot_sendFloat(objCounter);
+        cyBot_sendString("\n\r");
         // now find smallest object after scan is complete
         cyBot_sendString("\n\r=== SCAN COMPLETE ===\n\r");
         if(objCounter > 0) {
@@ -215,35 +232,52 @@ while(1){
             cyBot_sendString("Pointing servo to smallest object...\n\r");
             cyBOT_Scan(obj[smallestIdx].middleAngle, &scan);
             cyBot_sendString("Done!\n\r");
+
+            lcd_printf("Angle: %d\nDist: %d", obj[smallestIdx].middleAngle, obj[smallestIdx].distance);
+
+
+
+//            float smallobjectangle = obj[smallestIdx].middleAngle;
+//            float smallobjectdist = obj[smallestIdx].distance;
+//
+//            // turn roomba towards smallest object and move forward
+//            if(smallobjectangle < 90){ // object is to the right
+//                float turnAngle = 90 - smallobjectangle;  // Calculate how much right of center
+//                turn_right(sensor_data, turnAngle);
+//                move_forward(sensor_data, (((smallobjectdist)*10) - 180));
+//
+//                cyBot_sendString("Note: Turned right ");
+//                cyBot_sendFloat(turnAngle);
+//                cyBot_sendString(" degrees and moved forward.\n\r");
+//                lcd_printf("Turn R: %.1f\nMove: %.1f", turnAngle, (smallobjectdist*10 - 180)/10);
+//
+//            } else if(smallobjectangle > 90){ // object is to the left
+//                float turnAngle = smallobjectangle - 90;  // Calculate how much left of center
+//                turn_left(sensor_data, turnAngle);
+//                move_forward(sensor_data, (((smallobjectdist)*10) - 180));
+//
+//                cyBot_sendString("Note: Turned left ");
+//                cyBot_sendFloat(turnAngle);
+//                cyBot_sendString(" degrees and moved forward.\n\r");
+//                lcd_printf("Turn L: %.1f\nMove: %.1f", turnAngle, (smallobjectdist*10 - 180)/10);
+//
+//            } else { // object is straight ahead (angle â‰ˆ 90)
+//                move_forward(sensor_data, (((smallobjectdist)*10) - 180));
+//
+//                cyBot_sendString("Note: Object straight ahead, moved forward.\n\r");
+//                lcd_printf("Straight\nMove: %.1f", (smallobjectdist*10 - 180)/10);
+//            }
+
+
         }else{
             cyBot_sendString("No objects detected.\n\r");
         }
     } //end of if == m
 }
+oi_free(sensor_data);
 }
-//}
-//}
-//
-//
-//}
 
-//    int i;
-//    cyBOT_Scan_t scan;
-//    cyBot_sendString("Degrees\tPING Distance (cm)\n");
 
-////     sweep scans (0-180 degrees) and prints angle and distance from detected objects
-//    for(i = 0; i < 180; i+=2){
-//        cyBOT_Scan(i, &scan);
-//
-//        char angle[] = {};
-//        sprintf(angle, "%i\t", i);
-//        cyBot_sendString(angle);
-//
-//        char scanDistance[] = {};
-//        sprintf(scanDistance, "%f\n\r", scan.sound_dist);
-//        cyBot_sendString(scanDistance);
-//    }
-//}
 
 
 
@@ -261,8 +295,48 @@ void cyBot_sendString(char str[]) {
     void cyBot_sendFloat(float value) {
         char buffer[32];  // enough space for most floats
 
-        // Convert float to string (2 decimal places — change as needed)
+        // Convert float to string (2 decimal places ï¿½ change as needed)
         snprintf(buffer, sizeof(buffer), "%.2f", value);
 
         cyBot_sendString(buffer);
     }
+
+    //            //if(prevDist-currDist>30){
+    //                obj[objCounter].startAngle=i;
+    //
+    ////                lcd_printf("object detected");
+    //                cyBot_sendString("\n object detected at ");
+    //                cyBot_sendFloat(currDist);
+    //                cyBot_sendString("\n\r");
+    //                cyBot_sendString("Angle detected at \n\r");
+    //                cyBot_sendFloat(i);
+    ////                char angle[] = {};
+    ////                angle = i;
+    //
+    //            //}
+    //                if(prevDist-currDist<-30){
+    //                    cyBot_sendString("object ended \n\r");
+    //                    obj[objCounter].endAngle=i;
+    //
+    //
+    //                    objCounter++;
+    //                }
+    //                if(objCounter>0){
+    //                cyBot_sendString("Object ");
+    //                cyBot_sendFloat(objCounter);
+    //                cyBot_sendString("\nstartAngle:");
+    //                cyBot_sendFloat(obj[objCounter-1].startAngle);
+    //                cyBot_sendString("\n\r");
+    //                cyBot_sendString("endAngle:");
+    //                cyBot_sendFloat(obj[objCounter-1].endAngle);
+    //                cyBot_sendString("\n\r");
+    //                char scanDistance[32] = {};
+    //                sprintf(scanDistance, "%f\n\r", scan.sound_dist);
+    //                 cyBot_sendString(scanDistance);
+    //                 //            char angle[] ={};
+    ////            sprintf(angle, "%i\t",i);
+    ////            cyBot_sendString(angle);
+    ////
+    ////            char scanDistance[] = {};
+    ////            sprintf(scanDistance, "%f\n\r", scan.sound_dist);
+    ////                    cyBot_sendString(scanDistance);
